d8f06d188e4f2234e6f02f225995a48503b36367
danvelc <81994050+danvelc@users.noreply.github.com>
2026-04-29 18:45:52 -0500

    Fixed entrypoint

--- hybrid/docker-entrypoint.sh
+++ hybrid/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 chown freerad:freerad /etc/freeradius/certs/*.pem
 chmod 600 /etc/freeradius/certs/*.pem
 exec "$@"

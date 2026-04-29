fa02d21cce0a381a9ca21a679c304e14e75f850a
danvelc <81994050+danvelc@users.noreply.github.com>
2026-04-29 18:53:47 -0500

    Fixed and removed unused entrypoint

--- hybrid/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-chown freerad:freerad /etc/freeradius/certs/*.pem
-chmod 600 /etc/freeradius/certs/*.pem
-exec "$@"
